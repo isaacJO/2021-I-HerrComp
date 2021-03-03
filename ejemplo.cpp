256dbc7db32ebec4ced47cbe90597ea44a207b68
Isaac Rubiano <jrubianom@unal.edu.co>
2021-03-02 23:56:58 -0500

    actualizacion pruba 2 bla bla

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
+#include <cstdlib>
 
-int main(int argc, char **argcv){
-  std::cout << "Hola mundo !\n";
+// ./a out 10 -> argc = 2, argv = ["./a.out","10"]
+int main(int argc, char **argv){
+  int a =std::atoi(argv[1]);
 
+  a = 2*a;
+  
+  std::cout<< "a: " << a << std::endl;
   
   return 0;
 
